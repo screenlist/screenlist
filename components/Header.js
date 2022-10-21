4d460f716a65f105304772970d60fc06ad185018
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-21 18:54:11 +0200

    creates header for easier nav while developing

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,52 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Header.module.css'
+import SignOutButton from './SignOutButton'
 
 const Header = () => {
 	return(
 		<header>
-			<nav role="main menu">
+			<nav role='main menu'>
 				<ul>
 					<li>
-						<Link href="#">
-							<a>Screen List</a>
+						<Link href='/'>
+							<a title="Home">
+								<Image
+									src='/screenlist.svg'
+									alt="logo"
+									width={60}
+									height={50}
+								/>
+							</a>
 						</Link>
 					</li>
 					<li>
-						<Link href="#">
-							<a>Search</a>
+						<Link href='/films'>
+							<a>Films</a>
+						</Link>
+					</li>
+					<li>
+						<Link href='/series'>
+							<a>Series</a>
 						</Link>
 					</li>
 					<li>
 						<div>
-							<Link href="#">
+							<Link href='/contribute'>
 								<a>Contribute</a>
 							</Link>
 						</div>
+					</li>
+					<li>
 						<div>
-							<Link href="#">
-								<a>Sign In</a>
+							<Link href='/users'>
+								<a>Account</a>
 							</Link>
+						</div>
+					</li>
+					<li>
+						<div>
+							<SignOutButton/>
 						</div>
 					</li>
 				</ul>
