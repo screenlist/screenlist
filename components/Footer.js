65c4c741bd4036c97c21309330574c6aa398f899
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-24 19:06:52 +0200

    initialise the footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../styles/Header.module.css'
+
+const Footer = () => {
+	return "Footer here"
+}
+
+export default Footer
