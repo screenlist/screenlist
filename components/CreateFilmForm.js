75c9c53922264289298d9f96cd7a3a9e226fdae0
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-24 19:05:58 +0200

    refactores the helper card

--- components/CreateFilmForm.js
+++ components/CreateFilmForm.js
@@ -1,34 +1,11 @@
-import { useState, useEffect } from 'react';
-import * as Yup from 'yup';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useState, useEffect } from 'react'
+import * as Yup from 'yup'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { baseUrl } from '../utils/fetch'
+import SearchSimilarResources from './SearchSimilarResources'
 
 const CreateFilmForm = () => {
-	const baseUrl = process.env.NEXT_PUBLIC_SERVER
 	const [name, setName] = useState('')
-	const [similarFilms, setSimilarFilms] = useState([])
-	const [error, setError] = useState('')
-	const [loading, setLoading] = useState(false)
-
-	useEffect(() => {
-		async function getFilms(){
-			try{
-				setError('')
-				const response = await fetch(`${baseUrl}/search/quick?film=${name}`)
-				const data = await response.json()
-				console.log(response, data)
-				setLoading(true)
-				if(data){
-					setLoading(false)
-					setSimilarFilms(data)
-				}
-			} catch(err) {
-				setLoading(false)
-				setError(err.message)
-			}
-		}
-
-		getFilms()
-	}, [name])
 
 	const fields = {
 		name: '',
@@ -45,34 +22,30 @@ const CreateFilmForm = () => {
 	}
 
 	return (
-		<Formik
-			initialValues={fields}
-			validationSchema={Yup.object({
-				name: Yup.string().max(60, 'Names are too long').required('Required'),
-				trailerUrl: Yup.string(),
-				type: Yup.string().max(20, 'Invalid').required('Required'),
-				format: Yup.string().max(20, 'Invalid').required('Required'),
-				productionStage: Yup.string().max(20, 'Invalid').required('Required'),
-				runtime: Yup.number('Must be a number'),
-				logline: Yup.string().max(150, 'Logline is too long').required('Required'),
-				plotSummary: Yup.string().max(500, 'Plot summary is too long'),
-				releaseDate: Yup.string().length(10, 'Invalid date'),
-				initialPlatform: Yup.string(),
-				genres: Yup.array().required('Required')
-			})}
-			onSubmit={async (values, { setErrors }) => {
-				try{
-					const credentials = ""
-				} catch(err) {
-					setAuthError("Invalid email or password")
-				}
-			}}
-			handleChange={({value}) => {
-				console.log(value['name'])
-				setName(value['name'])
-			}}
-		>
-			<div>
+		<section>
+			<Formik
+				initialValues={fields}
+				validationSchema={Yup.object({
+					name: Yup.string().max(60, 'Names are too long').required('Required'),
+					trailerUrl: Yup.string(),
+					type: Yup.string().max(20, 'Invalid').required('Required'),
+					format: Yup.string().max(20, 'Invalid').required('Required'),
+					productionStage: Yup.string().max(20, 'Invalid').required('Required'),
+					runtime: Yup.number('Must be a number'),
+					logline: Yup.string().max(150, 'Logline is too long').required('Required'),
+					plotSummary: Yup.string().max(500, 'Plot summary is too long'),
+					releaseDate: Yup.string().length(10, 'Invalid date'),
+					initialPlatform: Yup.string(),
+					genres: Yup.array().required('Required')
+				})}
+				onSubmit={async (values, { setErrors }) => {
+					try{
+						const credentials = ""
+					} catch(err) {
+						setAuthError("Invalid email or password")
+					}
+				}}
+			>
 				<Form
 					onChange={(values) => {
 						if(values.target.name =='name'){
@@ -83,18 +56,7 @@ const CreateFilmForm = () => {
 					<h2>Add a new film</h2>
 
 					<label htmlFor="name">Title</label>
-					<Field name="name">
-						{
-							({field, form, meta}) => {
-								return (
-									<div>
-										<input type="text" {...field} />
-										{meta.touched && meta.error && <div>{meta.error}</div>}
-									</div>
-								)
-							}
-						}
-					</Field>
+					<Field name="name" type="text" />
 					<ErrorMessage name="name" />
 
 					<label htmlFor="trailerUrl">Trailer URL</label>
@@ -154,24 +116,9 @@ const CreateFilmForm = () => {
 
 					<button type="submit">Create</button>
 				</Form>
-				<section>
-					{
-						name.length > 0 ? 
-						<h3>Checking if "{name}" already exist?</h3> : 
-						<h3>Existing Title Checker</h3>
-					}
-					{error && <p>{error}</p>}
-					{ loading && <p>Loading...</p> }
-					{ similarFilms.length > 0 &&
-						<ul>
-							{
-								similarFilms.map((film) => (<li key={film.name}>{film.name}</li>))
-							}
-						</ul>
-					}
-				</section>
-			</div>
-		</Formik>
+			</Formik>
+			<SearchSimilarResources name={name} resource='films' />
+		</section>
 	)
 }
 
--- components/CreateSeriesForm.js
+++ components/CreateSeriesForm.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import * as Yup from 'yup';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { baseUrl } from '../utils/fetch'
+import SearchSimilarResources from './SearchSimilarResources'
 
 const CreateSeriesForm = () => {
+	const [name, setName] = useState('')
+
 	const fields = {
 		name: '',
 		trailerUrl: '',
@@ -20,85 +24,94 @@ const CreateSeriesForm = () => {
 	}
 
 	return (
-		<Formik
-			initialValues={fields}
-			validationSchema={Yup.object({
-				name: Yup.string().max(60, 'Names are too long').required('Required'),
-				trailerUrl: Yup.string(),
-				type: Yup.string().max(20, 'Invalid').required('Required'),
-				seasons: Yup.number().min(1, 'Must have a minimum of 1 season').required('Required'),
-				episodes: Yup.number(),
-				productionStage: Yup.string().max(20, 'Invalid').required('Required'),
-				episodeRuntime: Yup.number('Must be a number'),
-				logline: Yup.string().max(150, 'Logline is too long').required('Required'),
-				plotSummary: Yup.string().max(500, 'Plot summary is too long'),
-				releaseDate: Yup.string().length(10, 'Invalid date'),
-				finalEpisodeDate: Yup.string().length(10, 'Invalid date'),
-				originalPlatform: Yup.string(),
-				genres: Yup.array().required('Required')
-			})}
-			onSubmit={async (values, { setErrors }) => {
-				try{
-					const credentials = ""
-				} catch(err) {
-					setAuthError("Invalid email or password")
-				}
-			}}
-		>
-			<Form>
-				<label htmlFor="name">Title</label>
-				<Field name="name" type="text" />
-				<ErrorMessage name="name" />
+		<section>
+			<Formik
+				initialValues={fields}
+				validationSchema={Yup.object({
+					name: Yup.string().max(60, 'Names are too long').required('Required'),
+					trailerUrl: Yup.string(),
+					type: Yup.string().max(20, 'Invalid').required('Required'),
+					seasons: Yup.number().min(1, 'Must have a minimum of 1 season').required('Required'),
+					episodes: Yup.number(),
+					productionStage: Yup.string().max(20, 'Invalid').required('Required'),
+					episodeRuntime: Yup.number('Must be a number'),
+					logline: Yup.string().max(150, 'Logline is too long').required('Required'),
+					plotSummary: Yup.string().max(500, 'Plot summary is too long'),
+					releaseDate: Yup.string().length(10, 'Invalid date'),
+					finalEpisodeDate: Yup.string().length(10, 'Invalid date'),
+					originalPlatform: Yup.string(),
+					genres: Yup.array().required('Required')
+				})}
+				onSubmit={async (values, { setErrors }) => {
+					try{
+						const credentials = ""
+					} catch(err) {
+						setAuthError("Invalid email or password")
+					}
+				}}
+			>
+				<Form
+					onChange={(values) => {
+						if(values.target.name =='name'){
+							setName(values.target.value)
+						}
+					}}
+				>
+					<h2>Add a new series</h2>
 
-				<label htmlFor="productionStage">Production Stage</label>
-				<Field name="productionStage" type="text" />
-				<ErrorMessage name="productionStage" />
+					<label htmlFor="name">Title</label>
+					<Field name="name" type="text" />
+					<ErrorMessage name="name" />
 
-				<label htmlFor="trailerUrl">Trailer URL</label>
-				<Field name="trailerUrl" type="text" />
-				<ErrorMessage name="trailerUrl" />
+					<label htmlFor="productionStage">Production Stage</label>
+					<Field name="productionStage" type="text" />
+					<ErrorMessage name="productionStage" />
 
-				<label htmlFor="type">Type</label>
-				<Field name="type" type="text" />
-				<ErrorMessage name="type" />
+					<label htmlFor="trailerUrl">Trailer URL</label>
+					<Field name="trailerUrl" type="text" />
+					<ErrorMessage name="trailerUrl" />
 
-				<label htmlFor="seasons">Seasons</label>
-				<Field name="seasons" type="number" />
-				<ErrorMessage name="seasons" />
+					<label htmlFor="type">Type</label>
+					<Field name="type" type="text" />
+					<ErrorMessage name="type" />
 
-				
+					<label htmlFor="seasons">Seasons</label>
+					<Field name="seasons" type="number" />
+					<ErrorMessage name="seasons" />					
 
-				<label htmlFor= "runtime">Runtime</label>
-				<Field name= "runtime" type="number" />
-				<ErrorMessage name= "runtime" />
+					<label htmlFor= "runtime">Runtime</label>
+					<Field name= "runtime" type="number" />
+					<ErrorMessage name= "runtime" />
 
-				<label htmlFor="logline">Logline</label>
-				<Field name="logline" type="text" />
-				<ErrorMessage name="logline" />
+					<label htmlFor="logline">Logline</label>
+					<Field name="logline" type="text" />
+					<ErrorMessage name="logline" />
 
-				<label htmlFor="plotSummary">Plot Summary</label>
-				<Field name="plotSummary" as="textarea" />
-				<ErrorMessage name="plotSummary" />
+					<label htmlFor="plotSummary">Plot Summary</label>
+					<Field name="plotSummary" as="textarea" />
+					<ErrorMessage name="plotSummary" />
 
-				<label htmlFor="releaseDate">Release Date</label>
-				<Field name="releaseDate" type="date" />
-				<ErrorMessage name="releaseDate" />
+					<label htmlFor="releaseDate">Release Date</label>
+					<Field name="releaseDate" type="date" />
+					<ErrorMessage name="releaseDate" />
 
-				<label htmlFor="finalEpisodeDate">Final Episode Date</label>
-				<Field name="finalEpisodeDate" type="date" />
-				<ErrorMessage name="finalEpisodeDate" />
+					<label htmlFor="finalEpisodeDate">Final Episode Date</label>
+					<Field name="finalEpisodeDate" type="date" />
+					<ErrorMessage name="finalEpisodeDate" />
 
-				<label htmlFor="originalPlatform">Original Platform</label>
-				<Field name="originalPlatform" type="text" />
-				<ErrorMessage name="originalPlatform" />
+					<label htmlFor="originalPlatform">Original Platform</label>
+					<Field name="originalPlatform" type="text" />
+					<ErrorMessage name="originalPlatform" />
 
-				<label htmlFor="genres">Genres</label>
-				<Field name="genres" type="text" />
-				<ErrorMessage name="genres" />
+					<label htmlFor="genres">Genres</label>
+					<Field name="genres" type="text" />
+					<ErrorMessage name="genres" />
 
-				<button type="submit">Create</button>
-			</Form>
-		</Formik>
+					<button type="submit">Create</button>
+				</Form>
+			</Formik>
+			<SearchSimilarResources name={name} resource='series' />
+		</section>
 	)
 }
 
