fe7856ccae62f6ca77cc2ef7b96969df0f522c66
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-24 19:05:00 +0200

    initialises components

--- components/AddButton.js
+++ components/AddButton.js
@@ -2,9 +2,15 @@ import Link from 'next/link'
 
 const AddButton = (props) => {
 	const type = props.type
-	if(type = 'film'){
+	if(type == 'film'){
 		return(
 			<Link href='/films/add'>Add film</Link>
+		)
+	}
+
+	if(type == 'series'){
+		return(
+			<Link href='/series/add'>Add series</Link>
 		)
 	}
 }
---
+++ components/HeroCard.js
@@ -0,0 +1,11 @@
+import styles from '../styles/HeroCard.module.css'
+
+const HeroCard = () => {
+	return(
+		<section>
+			<p>All of South African cinema live here.</p>
+		</section>
+	)
+}
+
+export default HeroCard
