77d81955d76d78db818f6ec45ad87e43f683a575
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-25 17:13:23 +0200

    exposes two enviroment variable to the client

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [`${process.env.NEXT_PUBLIC_HOST}`]
+  },
+  env: {
+    NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST,
+    NEXT_PUBLIC_SERVER: process.env.NEXT_PUBLIC_SERVER
+  }
 }
 
 module.exports = nextConfig
