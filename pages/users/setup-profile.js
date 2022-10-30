4385cc9b8b773c66580445e40f5444ba96061da0
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-10-30 20:58:51 +0200

    route for setting up user profile

---
+++ pages/users/setup-profile.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import CreateUsernameBio from '../../components/CreateUsernameBio'
+
+
+export default function SetUpProfile() {
+	return (
+		<div>
+			<CreateUsernameBio/>
+		</div>
+	)
+}
