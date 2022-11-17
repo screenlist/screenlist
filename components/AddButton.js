7524f7bba751b8be9fc1e9ccc094f07daa66f4e3
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-11-17 20:05:10 +0200

    beautifies the add button

--- components/AddButton.js
+++ components/AddButton.js
@@ -4,13 +4,33 @@ const AddButton = (props) => {
 	const type = props.type
 	if(type == 'film'){
 		return(
-			<Link href='/films/add'>Add film</Link>
+			<Link href='/films/add'>
+				<a className="button-good">Add Film</a>
+			</Link>
 		)
 	}
 
 	if(type == 'series'){
 		return(
-			<Link href='/series/add'>Add series</Link>
+			<Link href='/series/add'>
+				<a className="button-good">Add Series</a>
+			</Link>
+		)
+	}
+
+	if(type == 'company'){
+		return(
+			<Link href='/companies/add'>
+				<a className="button-good">Add Company</a>
+			</Link>
+		)
+	}
+
+	if(type == 'person'){
+		return(
+			<Link href='/people/add'>
+				<a className="button-good">Add People</a>
+			</Link>
 		)
 	}
 }
