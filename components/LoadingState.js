a873e2f632db2c8dce5efc21add9fb59504d0122
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-11-24 17:55:13 +0200

    bug fixes and modifications

--- components/LoadingState.js
+++ components/LoadingState.js
@@ -1,3 +1,25 @@
-export default function LoadingState(){
-	return "Loading..."
+import styles from '../styles/LoadingState.module.css'
+export default function LoadingState({page}){
+	if(page){
+		return (
+			<div className={styles.wholePageContainer}>
+				<div role="loading state" className={styles['lds-ellipsis']}>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+		)
+	}
+	return (
+		<div className={styles.container}>
+			<div role="loading state" className={styles['lds-ellipsis']}>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	)
 }
