eb02fc2458f7a2b740e2291f7f73e24cdb18a781
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2022-12-23 18:45:06 +0200

    implements on demand isr

--- utils/fetch.js
+++ utils/fetch.js
@@ -1,5 +1,16 @@
 export const baseUrl = process.env.NEXT_PUBLIC_SERVER
 
+export const refreshPath = async (path, token) => {
+ 	await fetch(`${baseUrl}/refresh`, {
+ 		method: 'POST',
+		headers: {
+			AuthorizationToken: token,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({path: path})
+ 	})
+}
+
 export const getItems = async (resource) => {
 	if(resource == 'users'){
 		const response = await  fetch(`${baseUrl}/users`)
